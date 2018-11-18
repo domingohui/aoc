ac4ff0848b30d0818c90734f94b287750a6c33de
Domingo Hui <domingohui@gmail.com>
2018-11-18 10:12:20 -0800

    aoc 2017 day 1

---
+++ 2017/day1.hs
@@ -0,0 +1,24 @@
+import Data.Char
+
+captcha :: [(Int, Int)] -> Int
+captcha xs =
+  let takeIfEqual x y =
+        case x `compare` y of
+          EQ -> x
+          _ -> 0
+  in
+    case xs of
+      (x, y) : rest -> takeIfEqual x y + captcha rest
+      _ -> 0
+
+shift :: [Int] -> Int -> [Int]
+shift xs offset = second ++ first
+  where (first, second) = splitAt offset xs
+
+main :: IO ()
+main = do
+  input <- getLine
+  let ints = (map digitToInt input)
+  let offset = length ints `div` 2
+  let shifted = shift ints offset
+  print (captcha (zip ints shifted))
