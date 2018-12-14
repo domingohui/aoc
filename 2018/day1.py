3880de6bb3a978cb4ecb08162979840b2f402f06
Domingo <5943822+domingohui@users.noreply.github.com>
2018-12-13 23:53:01 -0800

    Update day1.py

--- 2018/day1.py
+++ 2018/day1.py
@@ -6,21 +6,12 @@ seen_sums = set()
 for i in sys.stdin:
     nums.append(int(i))
 
-def get_infinite_iter(xs):
-    L = len(xs)
-    index = 0
-    while True:
-        if index == L:
-            index = 0
-        yield xs[index]
-        index += 1
-
-it = get_infinite_iter(nums)
-
-for x in it:
-    acc += x
+i = 0
+while True:
+    acc += nums[x % len]
     if acc in seen_sums:
         print(acc)
         break
     else:
         seen_sums.add(acc)
+        i += 1
